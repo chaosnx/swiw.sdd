d05ad762badc9399459a2a4f0b938cfa0a533384
Jose Luis Cercos Pita <jlcercos@gmail.com>
2017-01-20 13:09:10 +0100

    Recovered old fashion stance names

--- scripts/general/poses.lua
+++ scripts/general/poses.lua
@@ -1,8 +1,13 @@
-if USE_STANCE == 0 then  -- USE_PISTOL_STANCE1
+-- Ensure the poses are already defined
+if PISTOL_STANCE1 == nil then
+    include "general/standard_commands.lua"
+end
+
+if USE_STANCE == PISTOL_STANCE1 then
     include "general/pistol_poses.lua"
-elseif USE_STANCE == 1 then  -- USE_ROCKET_STANCE1
-elseif USE_STANCE == 2 then  -- USE_RIFLE_STANCE1
-elseif USE_STANCE == 3 then  -- USE_RIFLE_STANCE2
-elseif USE_STANCE == 4 then  -- USE_WOOKIEE_STANCE
-elseif USE_STANCE == 5 then  -- USE_SBD_STANCE
+elseif USE_STANCE == ROCKET_STANCE1 then
+elseif USE_STANCE == RIFLE_STANCE1 then
+elseif USE_STANCE == RIFLE_STANCE2 then
+elseif USE_STANCE == WOOKIEE_STANCE then
+elseif USE_STANCE == SBD_STANCE then
 end
