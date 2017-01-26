b4a79873f57fff9c9b16df613fdfc99c2fff8ec4
Jose Luis Cercos Pita <jlcercos@gmail.com>
2017-01-26 20:32:12 +0100

    Fixed the squads, which take infinite radius during the construction

---
+++ objects3d/nullpoint.lua
@@ -0,0 +1,16 @@
+nullpoint = {
+    pieces = {
+        nullpiece = {
+            offset = {0.0, 0.01, 0.0},
+        },
+    },
+
+    radius = 0.01,
+    height = 0.01,
+    midpos = {0.0, 0.005, 0.0},
+    tex1 = "imp-i-rockettrooper1.dds",
+    tex2 = "imp-i-rockettrooper2.dds",
+    numpieces = 1,
+}
+
+return nullpoint
---
+++ scripts/imp_is.lua
@@ -0,0 +1,7 @@
+function script.Create()
+    Sleep(100)
+    Spring.SetUnitRadiusAndHeight(unitID, 0.01, 0.01)
+end
+
+function script.Killed(recentDamage, maxHealth)
+end
