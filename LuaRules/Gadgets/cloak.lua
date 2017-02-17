f14e44c30a8dc8c1266ef72d018ad642f1eb8a17
J.L. Cercos-Pita <jlcercos@gmail.com>
2017-02-17 10:00:57 +0100

    Added the possibility of changing the sight distance of units while cloaked

--- LuaRules/Gadgets/cloak.lua
+++ LuaRules/Gadgets/cloak.lua
@@ -1,13 +1,13 @@
 function gadget:GetInfo()
-	return {
-		name      = "Custom Cloak Handler",
-		desc      = "Changes how units decloak",
-		author    = "Gnome",
-		date      = "November 2009",
-		license   = "PD",
-		layer     = 0,
-		enabled   = true
-	}
+    return {
+        name      = "Custom Cloak Handler",
+        desc      = "Changes how units decloak",
+        author    = "Gnome",
+        date      = "November 2009",
+        license   = "PD",
+        layer     = 0,
+        enabled   = true
+    }
 end
 
 local GetAllUnits = Spring.GetAllUnits
@@ -18,6 +18,7 @@ local GetUnitPosition = Spring.GetUnitPosition
 local GetUnitsInCylinder = Spring.GetUnitsInCylinder
 local GetUnitStates = Spring.GetUnitStates
 local SetUnitCloak = Spring.SetUnitCloak
+local SetUnitSensorRadius = SetUnitSensorRadius
 local AreTeamsAllied = Spring.AreTeamsAllied
 
 if (gadgetHandler:IsSyncedCode()) then
@@ -26,75 +27,84 @@ local cloakable = {}
 local cloakingUnits = {}
 
 function gadget:Initialize()
-	--[[raw = include("gamedata/LuaConfigs/aura_defs.lua")
-	for name,defs in pairs(raw) do
-		local udid = UnitDefNames[name].id or nil
-		if(udid) then auras[udid] = defs end
-	end]]
+    --[[raw = include("gamedata/LuaConfigs/aura_defs.lua")
+    for name,defs in pairs(raw) do
+        local udid = UnitDefNames[name].id or nil
+        if(udid) then auras[udid] = defs end
+    end]]
 
-	for udid in pairs(UnitDefs) do
-		if(UnitDefs[udid].canCloak == true) then
-			cloakable[udid] = true
-			--Spring.Echo(UnitDefs[udid].name)
-		end
-	end
+    for udid in pairs(UnitDefs) do
+        if(UnitDefs[udid].canCloak == true) then
+            cloakable[udid] = true
+            --Spring.Echo(UnitDefs[udid].name)
+        end
+    end
 
-	-- Loops through the units, calling g:UnitFinished() on each of them
-	for _,uid in ipairs(GetAllUnits()) do
-		local teamID = GetUnitTeam(unitID)
-		local unitDefID = GetUnitDefID(unitID)
-		gadget:UnitFinished(unitID, unitDefID, teamID)
-	end
+    -- Loops through the units, calling g:UnitFinished() on each of them
+    for _,uid in ipairs(GetAllUnits()) do
+        local teamID = GetUnitTeam(unitID)
+        local unitDefID = GetUnitDefID(unitID)
+        gadget:UnitFinished(unitID, unitDefID, teamID)
+    end
 end
 
 function gadget:UnitFinished(uid, udid, tid)
-	if(cloakable[udid]) then
-		cloakingUnits[uid] = {udid = udid, tid = tid, cloaked = nil}
-	end
+    if(cloakable[udid]) then
+        cloakingUnits[uid] = {udid = udid, tid = tid, cloaked = nil}
+    end
 end
 
 function gadget:UnitDestroyed(uid, udid, tid, aid, adid, atid)
-	if(cloakingUnits[uid]) then
-		cloakingUnits[uid] = nil
-	end
+    if(cloakingUnits[uid]) then
+        cloakingUnits[uid] = nil
+    end
 end
 
 function gadget:GameFrame(n)
-	if(n % 15 < 1) then
-		for uid,cdat in pairs(cloakingUnits) do
-			local states = GetUnitStates(uid)
-			local cloaked = states.cloak
+    if(n % 15 < 1) then
+        for uid,cdat in pairs(cloakingUnits) do
+            local states = GetUnitStates(uid)
+            local cloaked = states.cloak
 
-			if(cloaked == true) and (cdat.cloaked == false) then
-				SetUnitCloak(uid, true, 0)
-				cloakingUnits[uid].cloaked = true
-			end
+            if(cloaked == true) and (cdat.cloaked == false) then
+                SetUnitCloak(uid, true, 0)
+                cloakingUnits[uid].cloaked = true
+            end
 
-			local ux, uy, uz = GetUnitPosition(uid)
-			local ud = UnitDefs[cdat.udid]
-			local team = cdat.tid
-			local decloakDistance = tonumber(ud.customParams.decloakdistance) or 500
+            local ux, uy, uz = GetUnitPosition(uid)
+            local ud = UnitDefs[cdat.udid]
+            local team = cdat.tid
+            local decloakDistance = tonumber(ud.customParams.decloakdistance) or 500
 
-			local nearbyUnits = GetUnitsInCylinder(ux,uz,decloakDistance)
+            local nearbyUnits = GetUnitsInCylinder(ux,uz,decloakDistance)
 
-			if(nearbyUnits) then
-				for _,nid in ipairs(nearbyUnits) do
+            if(nearbyUnits) then
+                for _,nid in ipairs(nearbyUnits) do
 
-					if(ValidUnitID(nid) and nid ~= uid) then
-						local nteam = GetUnitTeam(nid)
-						local areAllied = AreTeamsAllied(team, nteam)
-						if(nteam ~= team and areAllied == false) then
-							local ndid = GetUnitDefID(nid)
-							if(UnitDefs[ndid].canMove == true) then
-								SetUnitCloak(uid, false, decloakDistance)
-								cloakingUnits[uid].cloaked = false
-							end
-						end
-					end
-				end
-			end
-		end
-	end
+                    if(ValidUnitID(nid) and nid ~= uid) then
+                        local nteam = GetUnitTeam(nid)
+                        local areAllied = AreTeamsAllied(team, nteam)
+                        if(nteam ~= team and areAllied == false) then
+                            local ndid = GetUnitDefID(nid)
+                            if(UnitDefs[ndid].canMove == true) then
+                                SetUnitCloak(uid, false, decloakDistance)
+                                cloakingUnits[uid].cloaked = false
+                            end
+                        end
+                    end
+                end
+            end
+
+            -- Eventually modify the sight distance
+            if(ud.customParams.cloaksightdistance ~= nil) then
+                if(cloakingUnits[uid].cloaked) then
+                    SetUnitSensorRadius(uid, "los", tonumber(ud.customParams.cloaksightdistance))
+                else
+                    SetUnitSensorRadius(uid, "los", tonumber(ud.losRadius))
+                end
+            end
+        end
+    end
 end
 
 end -- end synced
