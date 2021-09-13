50500db6d2819881881dceac8f5939cd944cb2e8
ChaosNX <chaosnx@gmail.com>
2021-09-13 19:33:20 -0400

    add utils to rules

--- LuaRules/draw.lua
+++ LuaRules/draw.lua
@@ -1,1 +1,2 @@
+VFS.Include("LuaRules/Includes/utilities.lua", nil, VFS.ZIP)
 VFS.Include("LuaGadgets/gadgets.lua",nil, VFS.BASE)
--- LuaRules/main.lua
+++ LuaRules/main.lua
@@ -1,1 +1,4 @@
+--VFS.Include("LuaRules/Includes/utilities.lua", nil, VFS.ZIP)
+VFS.Include("LuaRules/Includes/utilities.lua", nil, VFS.ZIP)
 VFS.Include("LuaGadgets/gadgets.lua",nil, VFS.BASE)
+-- VFS.Include(Script.GetName() .. '/gadgets.lua', nil, VFS.ZIP_ONLY)
